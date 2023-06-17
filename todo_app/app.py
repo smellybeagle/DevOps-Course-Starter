94acc1c1a059e29195c8e2aa8bc2d47f6c7a2d70
smellybeagle <lvarnham@outlook.com>
2023-06-17 15:58:34 +0100

    exercise_1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-
+from flask import Flask, render_template
 from todo_app.flask_config import Config
-
 app = Flask(__name__)
-app.config.from_object(Config())
-
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
