3d125631394ae5adabcf0dc7e093e2ce6282da22
smellybeagle <lvarnham@outlook.com>
2023-06-17 16:14:38 +0100

    exercise_1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    def get_items():
+        def add_item(title):
+            return render_template('index.html',get_items=all,add_item=title)
 
 if __name__ == '__main__':
    app.run(debug = True)
