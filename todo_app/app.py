9d459448e57656099ef9739751044cf685881106
smellybeagle <lvarnham@outlook.com>
2023-06-22 12:44:36 +0100

    Changed Button method

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask import render_template
-#from flask import redirect,render_template ,request
+from flask import render_template,redirect, request, render_template_string
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items,add_item
+
 app = Flask(__name__)
 app.config.from_object(Config())
 
@@ -12,12 +12,13 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html',items=items)
+    #return render_template_string('''<form action="/add-todo" method="POST"><button type="submit" name="Submit" value="Submit">Submit</button></form>''')
 
-""" @app.route("/add-todo" , methods = ["POST"])
+@app.route("/add-todo" , methods = ["POST"])
 def add_todo():
     item_title= request.form.get("title")
     add_item(item_title)
-    return redirect('/') """
+    return redirect('/')
         
 if __name__ == '__main__':
    app.run(debug = True)
