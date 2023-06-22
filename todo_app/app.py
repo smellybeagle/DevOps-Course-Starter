7bcd7554e3ebc9981fe8b686fdce0085b58450c4
smellybeagle <lvarnham@outlook.com>
2023-06-22 10:36:42 +0100

    exercise_1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,8 @@
-from todo_app.data.session_items import add_item,get_items
-from flask import Flask, redirect,render_template ,request
+from flask import Flask
+from flask import render_template
+#from flask import redirect,render_template ,request
 from todo_app.flask_config import Config
-
+from todo_app.data.session_items import get_items
 app = Flask(__name__)
 app.config.from_object(Config())
 
@@ -12,11 +13,11 @@ def index():
     items = get_items()
     return render_template('index.html',items=items)
 
-@app.route("/add-todo" , methods = ["POST"])
+""" @app.route("/add-todo" , methods = ["POST"])
 def add_todo():
     item_title= request.form.get("title")
     add_item(item_title)
-    return redirect('/')
+    return redirect('/') """
         
 if __name__ == '__main__':
    app.run(debug = True)
