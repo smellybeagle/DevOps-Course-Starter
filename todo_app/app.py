a1e6379261279742754609ee40a29534c38fd648
smellybeagle <lvarnham@outlook.com>
2023-07-26 11:06:47 +0100

    First Main Commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from .flask_config import Config
-from .data.session_items import add_item
-from todo_app.data.session_items import add_item
-from todo_app.trello_items import get_lists, new_card
+#from .data.session_items import add_item
+#from todo_app.data.session_items import add_item
+from todo_app.trello_items import todolists, doinglists, donelists,new_card,move_done
 from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
+
 
 #import os, requests
 app = Flask(__name__)
@@ -14,13 +15,28 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     #retrieve_items = get_items()
-    retrieve_items = get_lists()
-    return render_template('index.html', items_list = retrieve_items)
-
+    #retrieve_items = get_lists()
+    todo_items = todolists()
+    doing_items = doinglists()
+    done_items = donelists()
+    return render_template('index.html', todo_list = todo_items, doing_list = doing_items, done_list = done_items)
 
 @app.route('/additem', methods = ["POST"])
 def add_new_item():
-    #new_todo = request.form.get('new-todo')
+    #create_card()
+    #name = request.form.get('name')
     new_card()
+    #request.form.get['new_todo']
     return redirect('/')
-#x = 'https://api.trello.com/1/cards/64b6455a4fbda32c1ce593c9?key=1a14974b67e88e68f859a2f5236feb25&token=ATTA43e926f02a2de06813db070512611484789561e3f6dfd4eba1539e5b5f32d3ea8B26C75B'
+
+@app.route('/movedoing', methods = ["POST"])
+def movedoing():
+    #create_card()
+    move_done()
+    return redirect('/')
+
+@app.route('/movetodone', methods = ["POST"])
+def movedone():
+    #create_card()
+    move_done()
+    return redirect('/')
--- todo_app/trello_items.py
+++ todo_app/trello_items.py
@@ -1,5 +1,7 @@
 import os, requests
 from flask import request
+import json
+#from flask import session
 
 
 trello_key = os.getenv("API_KEY")
@@ -8,41 +10,83 @@ trello_board_id = os.getenv("BOARD_ID")
 trello_list_id = os.getenv("LIST_ID")
 #trello_url = os.getenv("BASE_URL")
 trello_url = "https://api.trello.com/1/"
-new_card_url="https://api.trello.com/1/cards"
+new_card_url="https://api.trello.com/1/cards/"
 
 #trello_url_params = {"key": trello_key, "token": trello_token}
 
+url1 = 'https://api.trello.com/1/boards/649be24fcd4e7f514a246cb9/lists'
+url2= 'https://api.trello.com/1/lists/'
+payload = {'key': trello_key, 'token': trello_token, 'field': 'name'}
+
+
+response = requests.get(url1, params=payload)
+data = response.content
+data_dict=json.loads(data)
+
+#print(data_dict)
+# print(data_dict[0])
+# print(data_dict[1])
+# print(data_dict[1])
+
+todo= data_dict[0].get("id");
+doing= data_dict[1].get("id");
+done= data_dict[2].get("id");
+
+#print (todo)
+
+
+
+urldoing = url2 + doing + '/cards'
+payload1 = {'key': trello_key, 'token': trello_token, 'fields': 'name'}
+response1 = requests.get(urldoing, params=payload1)
+ddoing = response1.content
+dictdoing=json.loads(ddoing)
+
+urldone = url2 + done + '/cards'
+payload1 = {'key': trello_key, 'token': trello_token, 'fields': 'name'}
+response2 = requests.get(urldone, params=payload1)
+ddone = response2.content
+dictdone=json.loads(ddone)
+
+# print (urltodo)
+# print (urldoing)
+# print (urldone)
+
+#print(dicttodo[0].get("id"));
+# print(dicttodo[1])
+# print(dictdoing[0])
+# print(dictdoing[1])
+#print(dictdone)
+#print (dicttodo)
+def todolists():
+  urltodo = url2 + todo + '/cards'
+  payload1 = {'key': trello_key, 'token': trello_token, 'fields': 'name'}
+  response = requests.get(urltodo, params=payload1)
+  dtodo = response.content
+  dicttodo=json.loads(dtodo)
+  return dicttodo
+
+def doinglists():
+   return dictdoing
+
+def donelists():
+   return dictdone
+
+#print(dictdoing)
 headers = {"Accept": "application/json"}
 
-def get_lists():
-    get_lists_path = "boards/"+trello_board_id+"/lists"
-    #get_lists_path = "lists/649be24fcd4e7f514a246cc0/cards"
-    url = trello_url + get_lists_path
-    query_params = {"key": trello_key, "token": trello_token, "cards": "open", "card_fields": "idList,name,shortUrl"}
-    #query_params = {"key": trello_key, "token": trello_token, "cards": "open", "card_fields": "name"}
-    print(f"URL: {url}")
-    response = requests.get(url, headers=headers, params=query_params)
-    #response = requests.get(url, params=query_params)
-    #print("JSON response is...")
-    print(response.json())
-    response_json = response.json()
-      
-    cards_dict = []
-    for trello_list in response_json:
-
-        for each_card in trello_list['cards']:
-
-            cards_dict.append(each_card)
-
-    #print("JSON (cards_dict) response is...")
-    print(cards_dict)
-
-    return cards_dict
-
 def new_card():
-    url = new_card_url
-    #name = request.form.get('new-todo')
-    query_params = {"idList": trello_list_id,"key": trello_key, "token": trello_token, "name" : request.form.get('new-todo')}
-    print(f"URL: {url}")
-    requests.request("POST",url, headers=headers, params=query_params)
-    #print(response.text)
+     url = new_card_url
+     name = request.form.get('name')
+     query_params = {"idList": todo,"key": trello_key, "token": trello_token, "name" : name, "pos" : 'bottom'}
+     #print(f"URL: {url}")
+     requests.request("POST",url, headers=headers, params=query_params)
+     #print(response.text)
+
+def move_done():
+     url3 = 'https://api.trello.com/1/cards/' 
+     #name = request.form.get('new-todo')
+     query_params = {"idList": todo,"key": trello_key, "token": trello_token}
+     print(f"URL: {url3}")
+     requests.request("PUT",url3, headers=headers, params=query_params)
+     #print(response.text)
