07584c1767895c7b43bb94ba854294b685e147d6
smellybeagle <lvarnham@outlook.com>
2023-08-15 14:29:38 +0100

    Linked up view model to frontend

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,9 +1,11 @@
+from typing import ItemsView
 from flask import Flask
 
 
 from .flask_config import Config
 from todo_app.trello_items import init_trello, todolists, doinglists, donelists,new_card,move_doing,move_done
 from flask import Flask, render_template, request, redirect
+from todo_app.ViewModel import ViewModel
 
 
 app = Flask(__name__)
@@ -16,7 +18,9 @@ def index():
     todo_items = todolists()
     doing_items = doinglists()
     done_items = donelists()
-    return render_template('index.html', todo_list = todo_items, doing_list = doing_items, done_list = done_items)
+    item_view_model = ViewModel(todo_items, doing_items, done_items)
+    #return render_template('index.html', view_model=item_view_model)
+    return render_template('index.html', view_model = item_view_model)
 
 @app.route('/additem', methods = ["POST"])
 def add_new_item():
