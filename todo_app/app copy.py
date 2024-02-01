541cc698f454f4efe6bb3fb2f71c12bfc9b8edbd
smellybeagle <lvarnham@outlook.com>
2024-02-01 15:53:19 +0000

    updated ci.yml again!!!

---
+++ todo_app/app copy.py
@@ -0,0 +1,44 @@
+from typing import ItemsView
+from flask import Flask
+import os
+from todo_app.data.item import Item
+from .flask_config import Config
+#from todo_app.trello_items import init_trello, todolists, doinglists, donelists,new_card,move_doing,move_done
+from todo_app.mongo import init_mongo, todolists, doinglists, donelists,new_card,move_doing,move_done
+from flask import Flask, render_template, redirect
+from todo_app.view_model import ViewModel
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
+    
+    init_mongo()
+
+    @app.route('/')
+    def index():
+        todo_items = todolists()
+        doing_items = doinglists()
+        done_items = donelists()
+        item_view_model = ViewModel(todo_items, doing_items, done_items)
+        return render_template('index.html', view_model = item_view_model)
+
+    @app.route('/additem', methods = ["POST"])
+    def add_new_item():
+        new_card()
+        return redirect('/')
+
+    @app.route('/movedoing', methods = ["POST"])
+    def movedoing():
+        move_doing()
+        return redirect('/')
+
+    @app.route('/movedone', methods = ["POST"])
+    def movedone():
+        move_done()
+        return redirect('/')
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
