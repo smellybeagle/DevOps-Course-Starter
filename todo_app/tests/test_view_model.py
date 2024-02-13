5a0834972750f449cdba7a293a22815cc7fb63b9
smellybeagle <lvarnham@outlook.com>
2024-02-13 17:15:14 +0000

    changed test to work with fakemongo

--- todo_app/tests/test_view_model.py
+++ todo_app/tests/test_view_model.py
@@ -5,9 +5,9 @@ from todo_app.debugger import writelog
 
 def test_view_model_todos_method_only_returns_todo_items_and_not_other_statuses():
     # Arrange
-    todo_items = [Item("1", "A todo I just created", "To Do")]
-    doing_items = [Item("2", "A todo I created awhile ago", "In Progress")]
-    done_items = [Item("3", "A todo I finished ages ago", "Completed")]
+    todo_items = [Item("1", "A todo I just created", '',"To Do")]
+    doing_items = [Item("2", "A todo I created awhile ago", '',"In Progress")]
+    done_items = [Item("3", "A todo I finished ages ago",'', "Completed")]
     
     view_model = ViewModel(todo_items, doing_items, done_items)
     
@@ -18,12 +18,12 @@ def test_view_model_todos_method_only_returns_todo_items_and_not_other_statuses(
     # Assert
     assert len(tditems) == 1
     single_tditem = tditems[0]
-    assert single_tditem.status == "To Do"
+    assert single_tditem.Status == "To Do"
     
     assert len(doitems) == 1
     single_doitem = doitems[0]
-    assert single_doitem.status == "In Progress"
+    assert single_doitem.Status == "In Progress"
     
     assert len(dnitems) == 1
     single_dnitem = dnitems[0]
-    assert single_dnitem.status == "Completed"
+    assert single_dnitem.Status == "Completed"
