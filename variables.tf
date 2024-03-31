3f3ea33c6a3fa4bb7e3592fdefd65430507b5e8a
smellybeagle <lvarnham@outlook.com>
2024-03-31 20:53:03 +0100

    change to var

--- variables.tf
+++ variables.tf
@@ -1,18 +1,5 @@
-variable "prefix" {
-  default="production"
-}
-
 variable "SECRET_KEY" {
 }
-
-#variable "OAUTHADMIN" {
-#}
-
-#variable "MONGODB_CONN" {
-#}
-
-#variable "TEST_MONGODB_CONN" {
-#}
 
 variable "OAUTH_CLIENT_ID" {
 }
@@ -22,9 +9,6 @@ variable "OAUTH_CLIENT_SECRET" {
 
 variable "TEST_SECRET_KEY" {
 }
-
-#variable "TEST_OAUTHADMIN" {
-#}
 
 variable "TEST_OAUTH_CLIENT_ID" {
 }
