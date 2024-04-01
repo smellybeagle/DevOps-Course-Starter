492b4709ff07db905207b94459b476fc4acecfcd
smellybeagle <lvarnham@outlook.com>
2024-04-01 15:51:46 +0100

    change #3

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "SECRET_KEY" {
-| default  = "a2de8faa0d5ce1a6a7eb0b453a0a4efbee86e6e9"
+default  = "a2de8faa0d5ce1a6a7eb0b453a0a4efbee86e6e9"
 }
 
 variable "OAUTH_CLIENT_ID" {
-| default = "646acde489758ec76d82"
+default = "646acde489758ec76d82"
 }
 
 variable "OAUTH_CLIENT_SECRET" {
-| default = "ded4cc0ce52d0cbe3ccde576b6f88f9ede88227e"
+default = "ded4cc0ce52d0cbe3ccde576b6f88f9ede88227e"
 }
 
 variable "TEST_SECRET_KEY" {
-| default  = "a2de8faa0d5ce1a6a7eb0b453a0a4efbee86e6e9"
+default  = "a2de8faa0d5ce1a6a7eb0b453a0a4efbee86e6e9"
 }
 
 variable "TEST_OAUTH_CLIENT_ID" {
-| default = "c9606087e5387542d741"
+default = "c9606087e5387542d741"
 }
 
 variable "TEST_OAUTH_CLIENT_SECRET" {
-| default = "cd33277781168367feea26cebe11ff72c0023a0f"
+default = "cd33277781168367feea26cebe11ff72c0023a0f"
 }
 
